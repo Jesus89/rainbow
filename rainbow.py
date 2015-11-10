f740601b06cfede3b76f97ed4237629aa7fde9a6
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-11-10 07:52:16 +0100

    JSON-RPC 2.0 implemented

--- rainbow.py
+++ rainbow.py
@@ -31,12 +31,40 @@ def Singleton(cls):
     cls_w.__name__ = cls.__name__
     return cls_w
 
-import json
+
+class JSONRPCException(Exception):
+    def __init__(self, code, message, data=None):
+        self.code = code
+        self.message = message
+        self.data = data
 
 
-class ExampleError(Exception):
+class ParseError(JSONRPCException):
     def __init__(self):
-        super(ExampleError, self).__init__('Example', 1234)
+        JSONRPCException.__init__(self, -32700, 'Parse error')
+
+
+class InvalidRequest(JSONRPCException):
+    def __init__(self):
+        JSONRPCException.__init__(self, -32600, 'Invalid Request')
+
+
+class MethodNotFound(JSONRPCException):
+    def __init__(self):
+        JSONRPCException.__init__(self, -32601, 'Method not found')
+
+
+class InvalidParams(JSONRPCException):
+    def __init__(self):
+        JSONRPCException.__init__(self, -32602, 'Invalid params')
+
+
+class InternalError(JSONRPCException):
+    def __init__(self, data):
+        JSONRPCException.__init__(self, -32603, 'Internal error', data)
+
+
+import json
 
 
 @Singleton
@@ -46,31 +74,107 @@ class Rainbow(object):
         self.functions = {}
 
     def register(self, key, function):
+        """
+        Register function in dictionary with the provided key
+        """
         self.functions[key] = function
 
-    def call(self, key, args={}, kwargs={}):
-        return self.functions[key](*args, **kwargs)
+    def process_request(self, request):
+        """
+        Process request according to JSON-RPC 2.0 Specs <http://www.jsonrpc.org/specification>
+        Input and output data types are JSON string
+        """
+        try:
+            request = json.loads(request)
+        except:
+            # Parse error
+            response = {'jsonrpc': '2.0',
+                        'error': {'code': ParseError().code,
+                                  'message': ParseError().message},
+                        'id': None}
+        else:
+            if isinstance(request, list):
+                if not request:
+                    # Empty array
+                    response = {'jsonrpc': '2.0',
+                                'error': {'code': InvalidRequest().code,
+                                          'message': InvalidRequest().message},
+                                'id': None}
+                else:
+                    # Batch call
+                    response = []
+                    for req in request:
+                        res = self._single_request(req)
+                        if res is not None:
+                            # Not notification
+                            response += [res]
+                    if not response:
+                        # All notifications
+                        response = None
+            else:
+                # Single call
+                response = self._single_request(request)
+        finally:
+            if response is not None:
+                return json.dumps(response)
 
-    def json_rpc(self, request=None):
-        response = {u'jsonrpc': u'2.0'}
-        if self._verify_request(request):
-            if isinstance(request['params'], list):
-                args = request['params']
+    def _single_request(self, request):
+        try:
+            self._verify_request(request)
+            result = self._execute_request(request)
+        except JSONRPCException as e:
+            response = {'jsonrpc': '2.0',
+                        'error': {'code': e.code, 'message': e.message},
+                        'id': None}
+            if isinstance(e, InternalError):
+                response['error']['data'] = e.data
+            if isinstance(e, MethodNotFound):
+                response['id'] = request['id']
+        else:
+            if 'id' in request:
+                response = {'jsonrpc': '2.0',
+                            'result': result,
+                            'id': request['id']}
             else:
-                args = {}
-            if isinstance(request['params'], dict):
-                kwargs = request['params']
-            else:
-                kwargs = {}
-            response[u'result'] = self.call(request['method'], args, kwargs)
-            response[u'id'] = request['id']
-        return response
+                # Notification
+                response = None
+        finally:
+            return response
 
     def _verify_request(self, request):
-        return True
+        if not isinstance(request, dict) or \
+           'jsonrpc' not in request or \
+           request['jsonrpc'] != '2.0':
+            raise InvalidRequest
 
-    def json_str(self, data):
-        return json.dumps(json.loads(data))
+    def _execute_request(self, request):
+        if 'params' in request and isinstance(request['params'], list):
+            args = request['params']
+        else:
+            args = {}
+        if 'params' in request and isinstance(request['params'], dict):
+            kwargs = request['params']
+        else:
+            kwargs = {}
+        result = self.call(request['method'], args, kwargs)
+        return result
+
+    def call(self, key, args={}, kwargs={}):
+        if isinstance(key, unicode):
+            if key in self.functions:
+                try:
+                    result = self.functions[key](*args, **kwargs)
+                except TypeError as e:
+                    raise InvalidParams
+                except Exception as e:
+                    raise InternalError(e.message)
+                else:
+                    return result
+            else:
+                raise MethodNotFound
+        else:
+            raise InvalidRequest
+
 
 app = Rainbow()
 
