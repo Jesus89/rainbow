bced61ed9986cb69fb4b9f15fd7de35c13925da2
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-11-16 01:39:26 +0100

    Fixed tests

--- rainbow/dealer.py
+++ rainbow/dealer.py
@@ -7,7 +7,10 @@ __copyright__ = 'Copyright (c) 2015 Mundo Reader S.L.'
 __license__ = 'GPLv2'
 
 import json
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 from geventwebsocket import WebSocketApplication, WebSocketServer, Resource
 
 from rainbow.singleton import Singleton
