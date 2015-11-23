680e85e4614150c7dbaa2305be1ddd34226fe48e
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-11-23 18:07:27 +0100

    Version 0.0.5

--- rainbow/__init__.py
+++ rainbow/__init__.py
@@ -9,7 +9,7 @@ __email__ = 'jesus.arroyo@bq.com'
 __copyright__ = 'Copyright (c) 2015 Mundo Reader S.L.'
 __license__ = 'GPLv2'
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 try:
     from rainbow.dealer import Dealer
